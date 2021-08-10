749138339b11f80c032ab0b232733878e6bfde3b
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-08-10 16:03:28 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,19 +2,11 @@
 #include <Physics.h>
 #include <World.h>
 
-void Deform2D(Model& chunk, float offsetX, float offsetY, World& world, double strenght)
+void Deform(Model& chunk, float offsetX, float offsetY, World& world, double strenght)
 {
     for(int i = 0; i < chunk.numVerts * 3; i += 3)
     {
         chunk.vertexArray[i + 1] = world.GetNoise(std::abs(chunk.vertexArray[i] + offsetX), std::abs(chunk.vertexArray[i + 2] + offsetY)) * strenght;
-    }
-}
-
-void Deform3D(Model& chunk, float offsetX, float offsetY, float offsetZ, World& world, double strenght) //in progress
-{
-    for(int i = 0; i < chunk.numVerts * 3; i += 3)
-    {
-        chunk.vertexArray[i + 1] = world.GetNoise(std::abs(chunk.vertexArray[i] + offsetX), std::abs(chunk.vertexArray[i + 1] + offsetY), std::abs(chunk.vertexArray[i + 2] + offsetZ)) * strenght;
     }
 }
 
@@ -37,7 +29,7 @@ int main()
     engine.EventLoop([&](sf::Event& event) { if(event.type == sf::Event::Closed) engine.Close(); });
     
     Light l(GL_LIGHT0, 30, 30, 30);
-    Camera cam(0, 10, 0, 0.1);
+    Camera cam(world.Random(-512, 512), 10, world.Random(-512, 512), 0.1);
 
     Model chunk("data/models/chunk.obj", "data/textures/grass.jpg", 0, 0, 0);
 
@@ -46,7 +38,7 @@ int main()
     sf::Clock frametime, globaltime;
 
     l.SetParameters({ 0.05 }, GL_LINEAR_ATTENUATION);
-    l.SetParameters({ 2, 2, 2, 1 }, GL_AMBIENT);
+    l.SetParameters({ 3, 3, 3, 1 }, GL_AMBIENT);
 
     GLuint skybox[6] = {
         LoadTexture("data/skybox/skybox_front.bmp"),
@@ -83,10 +75,10 @@ int main()
         l.Update();
 
         cam.y -= 0.1;
-        for(int i = -4 + cam.z / 10; i < 4 + cam.z / 10; i++)
-            for(int j = -4 + cam.x / 10; j < 4  + cam.x / 10; j++)
+        for(int i = -2 + cam.z / 10; i < 2 + cam.z / 10; i++)
+            for(int j = -2 + cam.x / 10; j < 2  + cam.x / 10; j++)
             {
-                Deform2D(chunk, j * 10, i * 10, world, 7);
+                Deform(chunk, j * 10, i * 10, world, 7);
                 chunk.SetPosition(j * 10, 0, i * 10);
                 chunk.Draw();
                 cam.y += (collision(cam.x, cam.y, cam.z, chunk).second.y / 3);
